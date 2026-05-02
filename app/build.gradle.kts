4725679b4911efe150b25252d25094c65dc54577
jamdabam <test@fritz.box>
2026-05-02 22:33:43 +0200

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,10 +49,10 @@ android {
 dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2024.06.00")
 
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
-    implementation("androidx.activity:activity-compose:1.9.1")
+    implementation("androidx.core:core-ktx:1.18.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
+    implementation("androidx.activity:activity-compose:1.13.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation(composeBom)
     implementation("androidx.compose.ui:ui")
